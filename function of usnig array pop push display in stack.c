a704936615c15964f0b500d1265822eb3354815f
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2019-08-06 19:28:22 +0530

    funcytion

---
+++ function of usnig array pop push display in stack.c
@@ -0,0 +1,34 @@
+void push(){
+	if(top>=size)
+	{
+		printf("stack is overflow")
+	}
+	else
+	{
+		printf("\n enter the value");
+		scanf("%d",&stack [top]);
+		top++;
+	}
+}//end of push();
+
+void pop()
+{
+	if(top<=0)
+	{
+		printf("\n stack is overflow");
+		
+	}
+	else
+	top--;
+	printf("deleted value =%d",stack[top]);
+}// end of pop//
+
+void display()
+{
+	int i;
+	for(i=0;i<top;i++)
+	{
+		printf("\n %d",stack[i] );
+	}
+}
+// end of display///
